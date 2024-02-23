97d8bfbfc8e97ae9757fc42606dcb3614f4512c1
buchii <oko22007@byui.edu>
2024-02-23 21:23:27 +0100

    fixed urlParse issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,12 +39,9 @@ export const toDateString = (date: Date) => {
   });
 };
 
+
 export function getUrl() {
-  if (process.env.NODE_ENV === "development") {
-    return process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-  } else {
     return process.env.NEXT_PUBLIC_WEB_URL || "https://beetech-rho.vercel.app";
-  }
 }
 
 // BlurData for loading images with blur effect
